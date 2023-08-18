eac641aaffd75c01f3d6992b98100afa2bbdba8c
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-08-18 08:16:00 +0100

    figma

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const btnNAvElemant = document.querySelector(".btn-mobile-nav");
+const headerElement = document.querySelector(".header");
+
+btnNAvElemant.addEventListener("click", function() {
+    headerElement.classList.toggle("nav-open");
+})
